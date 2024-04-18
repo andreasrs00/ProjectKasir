8c53eeb23a88fc22c597e790c02154083fbd8605
Andreas Robson Simanjuntak <xandersimanjuntak@gmail.com>
2024-04-18 21:00:42 +0700

    product

---
+++ src/Product.java
@@ -0,0 +1,37 @@
+public class Product {
+    private String name;
+    private double price;
+    private int stock;
+
+    //Constructor
+    public Product(String name, double price, int stock){
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    //Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+}
---
+++ src/ProductManagement.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+public class ProductManagement {
+    private List<Product> ProductList;
+
+    //Constructor
+    public ProductManagement() {
+        ProductList = new ArrayList<>();
+    }
+
+    //Menambahkan produk baru
+    public void addProduct(){}
+}
+
