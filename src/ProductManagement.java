07af2fb4c7503b84703cbcd804877150f35be82a
Andreas Robson Simanjuntak <xandersimanjuntak@gmail.com>
2024-04-22 19:26:02 +0700

    update pmanagement

--- src/ProductManagement.java
+++ src/ProductManagement.java
@@ -1,14 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 public class ProductManagement {
-    private List<Product> ProductList;
+    private List<Product> productList;
 
     //Constructor
     public ProductManagement() {
-        ProductList = new ArrayList<>();
+        productList = new ArrayList<>();
     }
 
-    //Menambahkan produk baru
-    public void addProduct(){}
+    //add a new product
+    public void addProduct(Product product){
+        productList.add(product);
+    }
+
+    // delete products by name
+    public void deleteProduct(String nameProduct){
+        for (int i = 0; i < productList)
+    }
+
 }
 
